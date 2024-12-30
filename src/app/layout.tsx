f4f9ef9eaf9310d9bc4224afc679c9d7b04f1c5d
aliersej123@yahoo.com <ha@organicgrowth.dk>
2024-12-30 13:26:18 +0100

    Update layout metadata and format code in layout.tsx for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,39 +3,39 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
 } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shopify Product Description Assistant",
-  description: "Manage and improve your Shopify product descriptions",
+	title: "Content generator for organicgrowth ",
+	description: "Helps our customers generate content for their websites",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <Toaster position="top-center" />
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					<SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+					<Toaster position="top-center" />
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
